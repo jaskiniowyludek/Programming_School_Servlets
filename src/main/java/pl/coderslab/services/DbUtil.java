efea893043bd67ea4c5535fa876aef525755bfa7
Ewelina Pasternak <wulina@wp.pl>
2018-11-08 13:45:43 +0100

    connection to DB set up

---
+++ src/main/java/pl/coderslab/services/DbUtil.java
@@ -0,0 +1,24 @@
+package pl.coderslab.services;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DbUtil {
+
+    private	static	DataSource	ds;
+    public	static Connection getConn()	throws SQLException {
+        return	getInstance().getConnection();
+    }
+    private	static	DataSource	getInstance()	{
+        if(ds	==	null)	{
+            try	{
+                Context ctx	=	new InitialContext();
+                ds	=	(DataSource)ctx.lookup("java:comp/env/jdbc/Programming_School_Servlets");
+            }	catch	(NamingException e)	{
+                e.printStackTrace();}}
+        return	ds;}
+}
